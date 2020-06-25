7f02302e9c3f09c4fcd50a3ee3c7a550163e3f64
0surface <tms.0xvoid@gmail.com>
2020-06-25 09:22:06 +0100

    Added subject track tag helper, some css and bootstrap

--- TagHelpers.Lib/TagHelpers/Subject/SubjectCardTagHelper.cs
+++ TagHelpers.Lib/TagHelpers/Subject/SubjectCardTagHelper.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
-using AuthoringTagHelpers.Data;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TagHelpersLib.TagHelpers.Subject
 {
@@ -10,21 +6,27 @@ namespace TagHelpersLib.TagHelpers.Subject
     public class SubjectCardTagHelper : TagHelper
     {
         public AuthoringTagHelpers.Data.Subject Subject { get; set; }
+        private string  SourceLink = $"https://export.arxiv.org/list/#SubjectCode#/recent";
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            SourceLink = SourceLink.Replace("#SubjectCode#", Subject.Code);
             string content = $@"
-                            <div class='card' style='max-width:400px;max-height:200px'>                                
+                            <div class='card' style='width:320px;height:150px'>                                
                                 <div class='card-body'>
-                                    <h6 class ='card-title'>{Subject.Name}</h6>
-                                    <div class ='card-text'>{Subject.Code}</div>
+                                    <h6 class ='card-title' style='font-size:16px;font-weight:600;'>{Subject.Name}</h6>
+                                        <p class='card-subtitle mb-2 text-muted'>{Subject.GroupName}</p>
+                                          <div class ='card-text' style='font-size:10px;'>
+                                        <a href='{SourceLink.Replace("#SubjectCode#", Subject.Code)}' class='card-link' style='font-size:10px;' target='_blank'>{Subject.Code}</a>
+                                    </div>
                                 </div>
+                               <div class='card-footer' style='font-size:10px;'>{Subject.Discipline.ToUpper()}</div>
                             </div>
             ";
 
             output.TagName = "div";
             output.Content.SetHtmlContent(content);
-             //< div class='card-img'><img src = 'static/icons/cond-mat.mes-hall_nanotechnology.svg' ></ div >
+            //< div class='card-img'><img src = 'static/icons/cond-mat.mes-hall_nanotechnology.svg' ></ div >
         }
     }
 }
---
+++ TagHelpers.Lib/TagHelpers/Subject/SubjectTrackTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace TagHelpersLib.TagHelpers.Subject
+{
+    [HtmlTargetElement("arx-Subject-track")]
+    public class SubjectTrackTagHelper : TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            string preContent = @$"<div class='card-deck'>";
+
+            string postContent = @$"</div>";
+
+            output.TagName = "div";
+            output.Attributes.SetAttribute("class", "track");
+            output.PreContent.SetHtmlContent(preContent);
+            output.PostContent.SetHtmlContent(postContent);
+        }
+    }
+}
